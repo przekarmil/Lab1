e4502657423879e156525b1d90d58daff58b03f5
przekarmil <przekarmil@gmail.com>
2022-06-12 17:49:54 +0200

    10a - 10f

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,20 +2,29 @@ package pl.lublin.wsei.java.cwiczenia;
 
 import java.util.Scanner;
 
+//  Proszę usunąć modyfikator static z metody leftPad. Co się stanie? | Błąd
+//  proszę przywrócić static i zamienić public na private – co się stanie? | Nic
+
 public class Main {
+    public static String leftPad(String aText, char aChar, int aWidth){
+        String res = aText;
+        for (int i = 0; i < aWidth-aText.length(); i++) {
+            res = aChar + res;
+        }
+        char[] resArray = res.toCharArray();
+        for (int i = 0; i < resArray.length; i++) {
+            if(Character.isLowerCase(resArray[i])){
+                resArray[i] = Character.toUpperCase(resArray[i]);
+            }
+        }
+
+        return new String(resArray);
+    }
     public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-        int num1 = 0, num2 = 0;
-        do {
-            System.out.println("Podaj liczbę, które mam dodać: ");
-            num1 = input.nextInt();
-            num2 = input.nextInt();
-            if ((num1 == 0) || (num2 == 0)) break;
-            System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
-
-        } while (true);
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj Liczbe do konwersji: ");
+        int liczba = scanner.nextInt();
+        System.out.printf("DEC = %d\tBIN = %s\tHEX = %s\n",  liczba,
+                leftPad(Integer.toBinaryString(liczba), '0', 8 ), leftPad(Integer.toHexString(liczba), '0', 8));
     }
 }
-
