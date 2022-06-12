09b2f4faabe6bee132ec09e891ab4034bad43ee0
przekarmil <przekarmil@gmail.com>
2022-06-12 17:44:20 +0200

    1a - 7g

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,6 +2,9 @@ package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("alfa\tsin(alfa)\n");
+        for (int i = 0; i < 370; i+=10) {
+            System.out.printf("%d\t%f\n", i, Math.sin(i/360.0*2*Math.PI));
+        }
     }
 }
